8c4f7ee99cc5b7cf72017d6ea653e42bfd4756bb
Michael Williamson <mike@zwobble.org>
2022-12-14 22:03:06 +0000

    Update download URL on macos for wabt 1.0.30 and later

--- index.js
+++ index.js
@@ -4,18 +4,19 @@ const core = require("@actions/core");
 const exec = require("@actions/exec");
 const github = require("@actions/github");
 const toolCache = require("@actions/tool-cache");
+const semver = require("semver");
 
 function findArchive({version, nodePlatform}) {
-    const wabtPlatform = nodePlatformToWabtPlatform(nodePlatform);
+    const wabtPlatform = nodePlatformToWabtPlatform({nodePlatform, version});
     const directoryName = `wabt-${version}`;
 
     return [directoryName, `https://github.com/WebAssembly/wabt/releases/download/${version}/${directoryName}-${wabtPlatform}.tar.gz`]
 }
 
-function nodePlatformToWabtPlatform(nodePlatform) {
+function nodePlatformToWabtPlatform({nodePlatform, version}) {
     switch (nodePlatform) {
         case "darwin":
-            return "macos";
+            return semver.gte(version, "1.0.30") ? "macos-12" : "macos";
         case "linux":
             return "ubuntu";
         case "win32":
